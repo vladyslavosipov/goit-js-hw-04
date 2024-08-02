f8efd40e604ca8c04fd50c024e647464b0a36fb1
Vlad Osipov <vladyslav.osip@gmail.com>
2024-08-02 13:54:39 +0300

    add task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,21 @@
-function getElementWidth(content, padding, border) {
-    let contents = parseFloat(content);
-    let paddings = parseFloat(padding);
-    let borders = parseFloat(border);
-    return contents + paddings * 2 + borders * 2;
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        profile.username = newName
+    },
+    updatePlayTime(hours) {
+        profile.playTime += hours
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
 };
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
